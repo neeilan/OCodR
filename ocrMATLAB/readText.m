75c3fed52b1c6dbda33a67e313557074a6d1ca49
Aaruran Elamurugaiyan <aarurane@gmail.com>
2017-01-16 23:36:22 -0500

    Added wrapper function for MATLAB ocr function

---
+++ ocrMATLAB/readText.m
@@ -0,0 +1,9 @@
+function [ readText ] = readText( imageFile )
+%OCR function
+%   Reads in an image, and returns the observed text in it.
+I = imread(imageFile);
+x = ocr(I);
+readText = x.Text;
+
+end
+
